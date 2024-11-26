36f35ebaa13aaaf4ab64142ce016f1c884c7d20f
Viegas Adilson <adilsonviegas@hotmail.com>
2024-11-26 09:47:20 +0000

    cv page

---
+++ src/data/data-cv.js
@@ -0,0 +1,12 @@
+export const employment = [
+  {
+    placement: "GXO",
+    dateTime: "03/10/2023-Presente",
+    position: "Warehouse Operative",
+    skills: [
+      "Picking and packing,",
+      "Loading, unloading, goods in and dispatch",
+      "Supporting any additional duties as requested by department Managers,",
+    ],
+  },
+];
